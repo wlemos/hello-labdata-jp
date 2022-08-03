5b60170a22900bfbfd7d9b343dfd18b673a38cb6
wlemos <39625225+wlemos@users.noreply.github.com>
2022-08-02 21:47:29 -0300

    função de hello implementada

--- hello_labdata_jp/_modidx.py
+++ hello_labdata_jp/_modidx.py
@@ -26,4 +26,5 @@ d = { 'settings': { 'audience': 'Developers',
                 'tst_flags': 'notest',
                 'user': 'wlemos',
                 'version': '0.0.1'},
-  'syms': {'hello_labdata_jp\\core': {'hello_labdata_jp\\core.hello': 'https://wlemos.github.io/hello_labdata_jp/hello_labdata_jp\\core.html#hello'}}}
+  'syms': { 'hello_labdata_jp\\core': {'hello_labdata_jp\\core.hello': 'https://wlemos.github.io/hello_labdata_jp/hello_labdata_jp\\core.html#hello'},
+            'hello_labdata_jp\\greetings': { 'hello_labdata_jp\\greetings.hello': 'https://wlemos.github.io/hello_labdata_jp/hello_labdata_jp\\greetings.html#hello'}}}
